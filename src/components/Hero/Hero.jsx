2b04c2d122cbccf9ab671230c73faa0da1b8ba3b
Amos Augusto <amosaugustos@gmail.com>
2024-08-11 16:43:20 +0700

    feat: add some additional experiences

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Amos Augusto Silangit</h1>
                 <p className={styles.description}>I'm a full-stack developer with 2 years of experience using React, NodeJS, and Typescript. Reach out if you'd like to learn more!</p>
-                <a href="mailto:amosaugustos@gmail.com" className={styles.contactBtn}>Contact Me</a>
+                <a href="https://drive.google.com/file/d/1h2RAs2h-_3XeWS41T9rAtUpWsYtOdNga/view?usp=drive_link" className={styles.contactBtn}>Check Resume</a>
             </div>
             <img src={getImageUrl("hero/myself.png")} alt="Hero image of me" className={styles.heroImg} />
             <div className={styles.topBlur} />
